d2af3195dadb64b674b9a513e2d616df633b93f1
Hugh Jackson <hughjackson87@gmail.com>
2022-08-28 23:28:19 +0100

    Add package scoping into TB template

--- tests/test_enum/tb_template.sv
+++ tests/test_enum/tb_template.sv
@@ -7,12 +7,14 @@
     ##1;
 
     // check initial conditions
-    cpuif.assert_read('h0, top_my_enum_val_2);
+    cpuif.assert_read('h0, regblock_pkg::top_my_enum_val_2);
+    cpuif.assert_read('h0, 'h4');
 
     //---------------------------------
     // set r0 = val_1
-    cpuif.write('h0, top_my_enum_val_1);
+    cpuif.write('h0, regblock_pkg::top_my_enum_val_1);
 
-    cpuif.assert_read('h0, top_my_enum_val_1);
+    cpuif.assert_read('h0, regblock_pkg::top_my_enum_val_1);
+    cpuif.assert_read('h0, 'h0');
 
 {% endblock %}
