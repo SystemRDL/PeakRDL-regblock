fc823455f90e0c71f4022f8354a83ebcfa50a605
Alex Mykyta <amykyta3@users.noreply.github.com>
2023-05-03 23:22:05 -0700

    Add testcase to test cumulative strobes. #38

--- tests/test_write_buffer/testcase.py
+++ tests/test_write_buffer/testcase.py
@@ -1,5 +1,8 @@
 from ..lib.sim_testcase import SimTestCase
+from ..lib.cpuifs.passthrough import Passthrough
 
 class Test(SimTestCase):
+    cpuif = Passthrough() # test with bit strobes
+
     def test_dut(self):
         self.run_test()
